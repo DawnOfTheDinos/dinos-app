3dbb5b0815ed85a2b6b93c25c05c4c1d4a771fbd
moikapy <moikapy@devmoi.com>
2021-07-28 16:39:41 -0400

    @moikapy: update next.config.js
    Add watch option for nextjs to hot-reload within docker
    
    included base config for potential url rewrites

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   reactStrictMode: true,
+  devIndicators: {
+    autoPrerender: true,
+  },
+  webpackDevMiddleware: (config) => {
+    // Solve compiling problem via vagrant
+    config.watchOptions = {
+      poll: 1000, // Check for changes every second
+      aggregateTimeout: 300, // delay before rebuilding
+    };
+    return config;
+  },
+  async rewrites() {
+    return [];
+  },
 }
