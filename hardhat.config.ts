addf34c00c990561ae2b7914e2d9abf595a6ab39
Tai Benvenuti <tpbsworkshop@gmail.com>
2021-08-11 23:45:54 +0300

    Fixed hardhat compilation problem

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,10 @@
 import { HardhatUserConfig } from 'hardhat/types';
 import '@nomiclabs/hardhat-waffle';
 import '@nomiclabs/hardhat-ethers';
-import 'hardhat-typechain';
-import '@openzeppelin/hardhat-upgrades';
-
+import '@typechain/hardhat';
 const config: HardhatUserConfig = {
-  solidity: {
-    compilers: [{ version: "0.8.6", settings: {} }],
-  },
-};
-
+    solidity: {
+      compilers: [{ version: "0.8.0", settings: {} }],
+    },
+  };
 export default config;
