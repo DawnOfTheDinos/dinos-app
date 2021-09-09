8d40e9c38e68eb237db39d58d96c40613a0ab3c6
moikapy <moikapy@devmoi.com>
2021-09-08 23:31:13 -0400

    @moikapy: add modal, create dynamic roadmap,team,guest,and faq section

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,13 @@
+import Button from './Button';
 
-import Button from '../components/Button';
+interface INav {
+  address: string | null;
+}
+
 interface INavItem {
   Page: string;
 }
+
 async function handleEthereum() {
   const {ethereum}: any = window;
   if (ethereum && ethereum.isMetaMask) {
@@ -14,56 +19,60 @@ async function handleEthereum() {
   }
 }
 function NavItem(props: INavItem) {
-  return <div>{props.Page}</div>;
+  return <div className={`nav-item d-none d-xl-block`}>{props.Page}</div>;
 }
 
-function navbar() {
+function navbar(props: INav) {
   return (
     <>
       <style jsx>{`
         .navbar {
           color: #fff;
           background-color: #00242c;
-          height: 75px;
+          min-height: 75px;
         }
       `}</style>
       <nav className='navbar navbar-expand-lg d-flex flex-row align-items-center px-3'>
-        <div className={`w-100 d-flex flex-row justify-content-between align-items-center`}>
-        <a
-          className='text-uppercase h-100 text-decoration-none text-white'
-          href='/'>
-          Dawn of the dinos
-        </a>
-        <NavItem Page='About' />
-        <NavItem Page='Roadmap' />
-        <NavItem Page='Team' />
-        <NavItem Page='Dinos' />
-        <NavItem Page='Mint Yours' />
-        <NavItem Page='FAQ' />
-        <NavItem Page='Your Dinos' />
-        <Button
-        buttonStyle={`text-white btn-outline-success`}
-            onPress={async () => {
-              
-              if (window.ethereum !== undefined) {// eslint-disable-line
-                handleEthereum();
-              } else {
-                window.addEventListener(
-                  'ethereum#initialized',
-                  handleEthereum,
-                  {
-                    once: true,
-                  }
-                );
+        <div
+          className={`w-100 d-flex flex-row justify-content-between align-items-center`}>
+          <a
+            className='text-uppercase h-100 text-decoration-none text-white'
+            href='/'>
+            Dawn of the dinos
+          </a>
+          <NavItem Page='About' />
+          <NavItem Page='Roadmap' />
+          <NavItem Page='Team' />
+          <NavItem Page='Dinos' />
+          <NavItem Page='Mint Yours' />
+          <NavItem Page='FAQ' />
+          <NavItem Page='Your Dinos' />
+          {props.address == null ? (
+            <Button
+              buttonStyle={`text-white btn-outline-success`}
+              onPress={async () => {
+                if (window.ethereum !== undefined) {
+                  // eslint-disable-line
+                  handleEthereum();
+                } else {
+                  window.addEventListener(
+                    'ethereum#initialized',
+                    handleEthereum,
+                    {
+                      once: true,
+                    }
+                  );
 
-                // If the event is not dispatched by the end of the timeout,
-                // the user probably doesn't have MetaMask installed.
-                setTimeout(handleEthereum, 3000); // 3 seconds
-              }
-            }}>
-            Connect
-          </Button>
-      
+                  // If the event is not dispatched by the end of the timeout,
+                  // the user probably doesn't have MetaMask installed.
+                  setTimeout(handleEthereum, 3000); // 3 seconds
+                }
+              }}>
+              Connect
+            </Button>
+          ) : (
+            <div>{props.address}</div>
+          )}
         </div>
       </nav>
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import {ethers} from 'ethers';
-
 import {useEffect, useState} from 'react';
 
 import Image from 'next/image';
 import Button from '../components/Button';
 import Navbar from '../components/navbar';
+import Modal from '../components/modal';
 import DinoWeb3 from '../lib/DinoWeb3';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+const faqJSON = require('../src/data/faq.json');
+const roadmapJSON = require('../src/data/roadmap.json');
+const teamJSON = require('../src/data/team.json');
+const guestsJSON = require('../src/data/guests.json');
+
 function truncateAddress(address: string) {
   try {
     return `${address.substring(0, 6).toLowerCase()}...${address
@@ -33,6 +39,8 @@ export default function _index() {
     show: false,
   });
   let _web3: any, _signer: any, _DinoWeb3: any;
+  const [show, setShow] = useState(false);
+  const [num_of_dinos, setNumOfDinos] = useState<any>(0);
   useEffect(() => {
     Window = window;
     async function init() {
@@ -80,10 +88,26 @@ export default function _index() {
           html,
           body,
           #__next {
-            font-family: monospace;
+            font-family: Inter;
             font-size: 1.2rem;
             height: 100%;
             background-color: #00242c;
+          }
+          p,
+          li {
+            list-style: none;
+            font-size: 1rem;
+          }
+          .secondary-text-color {
+            color: #03dac5;
+          }
+          .trail-text {
+            color: #fff;
+            font-size: 2rem;
+            font-weight: bold;
+            z-index: 4;
+            top: 30px;
+            left: 35px;
           }
 
           .minting-section {
@@ -106,20 +130,36 @@ export default function _index() {
             background-position: center center;
           }
           .header-section-text {
-            width: 654px;
-            height: 515px;
+            max-width: 654px;
+            min-height: 515px;
           }
+
           .mint-button-style {
             background: #03dac5;
             color: #000;
           }
+
+          .modal-body {
+            width: 512px;
+            height: 562px;
+            color: #fff;
+            background-color: #00242c;
+          }
+          @media (max-width: 768px) {
+            .header-section-text > p {
+              font-size: 18px;
+            }
+          }
         `}
       </style>
-      <Navbar />
+      <Navbar address={truncateAddress(state.address)} />
       <div
-        className={`header-section d-flex flex-column justify-content-center align-items-center`}>
-        <span className={`header-section-text text-center`}>
-          <h1>Welcome to the Dawn of Time</h1>
+        className={`header-section d-flex flex-column justify-content-center align-items-center px-3`}>
+        <span className={`header-section-text text-center w-100`}>
+          <h1 className={`mb-4 text-capitalize`}>
+            Welcome to the Dawn
+            <br /> of Time
+          </h1>
           <p>
             Dawn of the Dinos is an Ethereum-based NFT collection of your
             favorite prehistoric creatures. Collecting one or more of these
@@ -133,17 +173,18 @@ export default function _index() {
             enjoy.
           </p>
           <Button
-            buttonStyle={`mint-button-style text-uppercase px-5`}
+            buttonStyle={`mint-button-style text-uppercase mt-4 px-5`}
             onPress={async () => {
               //Open Modal
-              alert('Minting');
+              setShow(true);
               // _DinoWeb3.mint();
             }}>
             Mint Your Dino
           </Button>
         </span>
       </div>
-      <div className={`collection-info-section  d-flex flex-column m-5`}>
+      <div
+        className={`container mx-auto collection-info-section  d-flex flex-column m-5 px-3`}>
         <h2 className={`text-capitalize text-white`}>The Collection</h2>
         <p className={`text-white`}>
           With 10,000 unique Dinos available to the public, there’s plenty of
@@ -159,7 +200,7 @@ export default function _index() {
           <li>Parasaurolophus</li>
           <li>Pachycephalosaur</li>
         </ul>
-        <p className={`text-white`}>
+        <p className={`text-white fw-lighter`}>
           Oh wait, that was only six different Dinos. What gives? Well Jimmy,
           <br />
           the seventh Dino is an ultra rare species that will emerge from the
@@ -169,7 +210,7 @@ export default function _index() {
           It gets better! We’ve manifested a total of 1600 Dinos per species!
           This includes a whopping total of 400 of our top secret Dinos!
         </p>
-        <p className={`text-white`}>Are you not impressed?</p>
+        <p className={`text-white fw-lighter`}>Are you not impressed?</p>
         <p className={`text-white`}>
           With more than 100 possible traits and well over 300 million
           combinations, there’s plenty of Dino fun to go around! So why not pick
@@ -177,63 +218,137 @@ export default function _index() {
           with us!
         </p>
       </div>
-      <div className={`dino-trail-section  d-flex flex-column m-5`}>
+      <div
+        className={`container mx-auto dino-trail-section  d-flex flex-column m-5 px-3`}>
         <h2 className={`text-capitalize text-white`}>Dino Trail</h2>
-        <h3 className={`text-capitalize text-white`}>Roadmap</h3>
+        <h4 className={`text-capitalize secondary-text-color`}>Roadmap</h4>
         <p className={`text-white`}>
           With 10,000 unique Dinos available to the public, there’s plenty of
           room for variety. Unlike some projects who boast an individual
           character, Dawn of the Dinos will showcase seven different species!
           The collective includes fan favorites like the:
         </p>
-        <ol className={`text-white`}>
-          <li className={`my-2`}>
-            We’re so happy to see our Dinos find new homes. So, kicking things
-            off at 30% sales, we will be setting up a community wallet filled
-            with 20 ETH. Later on, at 90% sales, we’ll throw in another 30 ETH!
-          </li>
-          <li className={`my-2`}>
-            Family is important to all of us — so much so, that this project was
-            inspired by our very own children! It’s important to include the
-            little tykes as often as possible, which is why we’re proud to
-            announce that we will be the FIRST ever collection to launch a
-            children’s book series alongside our Dinos! The first book will be
-            sent to our publishers at 50% sales.
-          </li>
-          <li className={`my-2`}>
-            Right around the corner at 60% sales, get ready to get your Dinos
-            on! Quite literally, ON! Merchandise will be put through the
-            printers! Everything from a T-shirt to a Hoodie. Gotta have them
-            signature Dad hats too! And an ample abundance of STICKERS. Again,
-            we’re family friendly. YIPPEE!
-          </li>
-          <li className={`my-2`}>
-            In the event of us all doing something remarkable and giving every
-            single Dino a new home, a very special surprise will be announced to
-            the happy new owners of cute little Dino eggs. Stay tuned!
-          </li>
-          <li className={`my-2`}>
-            Being incharge of the project and running it to its greatest
-            potential is of utmost importance to us. This is why we will be
-            adding a few of you into the fray. A select few of the owners will
-            be voted into the Dino Delegation! They will focus on one thing and
-            one thing only; to ensure the projects growth through the community.
-            They will be directly involved in future efforts to increase
-            community outreach and will work hand in hand with the original Dawn
-            of the Dinos team creating influence and longevity along the way!
-          </li>
-          <li className={`my-2`}>
-            Regardless of how well we do in the long run, community is
-            important. We strive to make our community a family worth fighting
-            for. We will not only be giving back to the community with multiple
-            curated drops and merchandise, but we feel it is our duty to give
-            back to humanity. Without the existence of Dinos, we wouldn’t be
-            here right now. A healthy donation will be made to help the fight
-            against cancer. We will announce our organization of choice post
-            launch.
-          </li>
-        </ol>
+
+        {roadmapJSON.map((item: any, index: number) => {
+          return (
+            <div
+              className={`my-4 text-white d-inline-flex flex-row justify-content-between align-items-center w-100`}>
+              <span className={'position-relative'}>
+                <p className={'position-absolute trail-text'}>{index + 1}</p>
+                <Image
+                  className={`col position-position`}
+                  height={'100px'}
+                  width={'100px'}
+                  layout='fixed'
+                  src={'/img/foot.png'}
+                />
+              </span>
+              <p className={'col-8'}>{item.track}</p>
+            </div>
+          );
+        })}
       </div>
+
+      <div
+        className={`container mx-auto team-section  d-flex flex-column m-5 px-3`}>
+        <h2 className={`text-capitalize text-white`}>The Team</h2>
+        <div
+          className={`d-flex flex-row flex-wrap justify-content-center mt-5`}>
+          {teamJSON.map((item: any, index: number) => {
+            return (
+              <div
+                className={`d-flex flex-column justify-content-center align-items-center m-2 mx-md-3`}>
+                <div>
+                  <Image
+                    height={'236px'}
+                    width={'236px'}
+                    layout='fixed'
+                    src={`/img/placeholderimg.png`}
+                  />
+                </div>
+                <div className='text-center'>
+                  <p className={`text-white`}>{item.name}</p>
+                  <p className={`text-white`}>{item.role}</p>
+                  <p className={`text-white`}>{item.title}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <h3 className={`text-capitalize text-white`}>Guest Starring</h3>
+        <div
+          className={`d-flex flex-row flex-wrap justify-content-center mt-5`}>
+          {guestsJSON.map((item: any, index: number) => {
+            return (
+              <div
+                className={`d-flex flex-column justify-content-center align-items-center m-2 mx-md-3`}>
+                <div>
+                  <Image
+                    height={'236px'}
+                    width={'236px'}
+                    layout='fixed'
+                    src={`/img/placeholderimg.png`}
+                  />
+                </div>
+                <div className='text-center'>
+                  <p className={`text-white`}>{item.name}</p>
+                  <p className={`text-white`}>{item.role}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div
+        className={`container mx-auto dinos-section  d-flex flex-column m-5 px-3`}>
+        <h2 className={`text-capitalize text-white`}>Dinos</h2>
+      </div>
+      <div
+        className={`container mx-auto faq-section  d-flex flex-column m-5 px-3`}>
+        <h2 className={`text-capitalize text-white`}>F.A.Q.</h2>
+        {faqJSON.map(({title, content}: {title: string; content: string}) => (
+          <div className={`text-capitalize text-white my-3`}>
+            {title}
+            <p className={`mt-3`}>{content}</p>
+          </div>
+        ))}
+      </div>
+      {show && (
+        <Modal
+          onClose={() => setShow(false)}
+          bodyStyle={`rounded d-flex flex-column justify-content-between
+            align-itens-center p-5`}>
+          <h1>How many Dinos would you like to mint?</h1>
+          <div className={`d-flex flex-column`}>
+            Number of Dinos: {num_of_dinos}
+            <label htmlFor='minter-input'></label>
+            <input
+              id={`minter-input`}
+              type={`range`}
+              min={0}
+              max={10}
+              value={num_of_dinos}
+              onChange={(e) => {
+                parseInt(e.target.value, 10) >= 0 &&
+                  parseInt(e.target.value, 10) <= 10 &&
+                  setNumOfDinos(e.target.value);
+              }}
+            />
+            This is the number of Dinos you intend to mint. Maximum of 10 per
+            transaction.
+          </div>
+          <Button
+            buttonStyle={`mint-button-style text-uppercase mt-4 px-5`}
+            onPress={async () => {
+              //Open Modal
+              alert('Minting');
+              setShow(false);
+              // _DinoWeb3.mint(num_of_dinos);
+            }}>
+            Mint
+          </Button>
+        </Modal>
+      )}
     </div>
   );
 }
